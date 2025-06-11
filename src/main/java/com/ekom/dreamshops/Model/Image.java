14b7172e9cf34072ebffc57ff00d5326a646bdff
aBugInTheSystem <ekom338.com@gmail.com>
2025-06-11 16:17:16 -0400

    Models created.

--- src/main/java/com/ekom/dreamshops/Model/Image.java
+++ src/main/java/com/ekom/dreamshops/Model/Image.java
@@ -1,4 +1,26 @@
 package com.ekom.dreamshops.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Blob;
+
+@Data
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    public Image() {
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
--- src/main/java/com/ekom/dreamshops/Model/Product.java
+++ src/main/java/com/ekom/dreamshops/Model/Product.java
@@ -1,4 +1,30 @@
 package com.ekom.dreamshops.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    public Product() {
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
 }
